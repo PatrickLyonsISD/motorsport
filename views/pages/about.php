9b78cc2e3cf38e6fa953d9c9b2a5c385c794383c
PatrickLyonsISD <43409140+PatrickLyonsISD@users.noreply.github.com>
2021-07-20 16:48:45 +0100

    first commit

---
+++ views/pages/about.php
@@ -0,0 +1,5 @@
+<div class="container">
+<?php $this->load->view('templates/ms'); ?>
+<h2><?=$title?></h2>
+<p>Welcome to motorsport Ireland, a blog where you can post about all things cars or any questions or problems you have and the community may be able to help.</p>
+</div>
---
+++ views/pages/home.php
@@ -0,0 +1,6 @@
+<div class="container">
+<?php $this->load->view('templates/ms'); ?>
+<h2><?=$title?></h2>
+<p>welcome to motorsport ireland blog. Feel free to sign up and post and or just check out our blog and comment on ours and other peoples posts.  </p>
+</div>
+
